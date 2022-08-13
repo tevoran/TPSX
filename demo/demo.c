51a082a5b85f0884c7014dddfca98666ffd52143
tevoran <j.pfeiffer93@gmx.de>
2022-08-13 13:29:38 +0200

    basic demo setup

--- demo/demo.c
+++ demo/demo.c
@@ -1,7 +1,34 @@
 #include <tpsx.h>
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <time.h>
 
 int main()
 {
-	printf("hello world\n");
+	if(0 != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+	{
+		printf("[ERROR] couldn't initialize SDL2");
+	}
+	SDL_Window *window = SDL_CreateWindow(
+		"TPSX demo",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		640,
+		480,
+		0);
+
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+	//show the rendered result
+	SDL_UpdateWindowSurface(window);
+
+	//saving the screenshot
+	char filename[100];
+	struct timespec ts;
+	timespec_get(&ts, TIME_UTC);
+	sprintf(filename, "%li.bmp", ts.tv_sec);
+	SDL_SaveBMP(surface, filename);
+
+	//quit
+	SDL_Quit();
+	return 0;
 }
